146d528ba8f413e364d44a109861372e4201ac0b
arckenos <73735400+arckenos@users.noreply.github.com>
2022-03-19 16:39:54 -0700

    cosas
    cosas variadas

---
+++ app/src/main/java/equipo3/examen/Car.kt
@@ -0,0 +1,13 @@
+package equipo3.examen
+
+class Car {
+    companion object {
+        lateinit var compra : ArrayList<Product>
+    }
+
+    init {
+        compra = ArrayList<Product>()
+    }
+
+
+}
