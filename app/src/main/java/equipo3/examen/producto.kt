eddf30cee66ffab9b5e1caa05457233a427928e8
MariaEncinasCar <90160554+MariaEncinasCar@users.noreply.github.com>
2022-03-19 18:26:02 -0700

    producto avance
    Prueba

---
+++ app/src/main/java/equipo3/examen/producto.kt
@@ -0,0 +1,11 @@
+package equipo3.examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class producto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_producto)
+    }
+}
