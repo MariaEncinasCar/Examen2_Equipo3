93bd374004e30ea70f8ffb04b92e0e208ff71ba1
arckenos <73735400+arckenos@users.noreply.github.com>
2022-03-19 17:04:36 -0700

    imagenes
    agregadas imagenes

--- app/src/main/java/equipo3/examen/menu.kt
+++ app/src/main/java/equipo3/examen/menu.kt
@@ -2,10 +2,24 @@ package equipo3.examen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val btnTacos : Button = findViewById(R.id.button_tacos)
+        val btnCaldos : Button = findViewById(R.id.button_caldos)
+        val btnAntojitos : Button = findViewById(R.id.button_antojitos)
+        val btnTortas : Button = findViewById(R.id.button_tortas)
+        val btnSopas : Button = findViewById(R.id.button_sopas)
+        val btnDrinks : Button = findViewById(R.id.button_drinks)
+        val btnEspecialidades : Button = findViewById(R.id.button_especialidades)
+        val btnSides : Button = findViewById(R.id.button_sides)
+
+
+
+
     }
 }
